c09f901002d167cf2f0980f126b4caf279e2d3b3
AlK <nikonp123@gmail.com>
2022-12-18 11:29:48 +0200

    init commit

---
+++ src/exampleFetch.tsx
@@ -0,0 +1,21 @@
+  // useEffect(():void => {
+  //   const fetchTest = async () => {
+  //     const ourUrl='http://belosvet.tools.dp.ua/obuchenie/hs/Orders/'
+  //     const youUsername ='test';
+  //     const youPassword = '159';  
+  //     // new Buffer(data)    
+  //     // const token = Buffer.from(`${youUsername}:${youPassword}`).toString('base64');        
+  //     const basicAuth = `Basic ${youUsername}:${youPassword}`;
+  //     // const basicAuth = 'Basic ' + token;
+  //     // console.log(basicAuth);
+
+  //     const res = await axios.get(ourUrl,
+  //     {  
+          
+  //       // headers: {  'Authorization': basicAuth},
+  //       auth: { username: youUsername, password: youPassword },
+  //     });
+  //     // console.log(res);
+  //   };
+  //   fetchTest();
+  // },[])
